8c0f1fc5d295bffdec0378f48ba670a638a69890
ClaraAntonacci <andrzejewskyantonacciclara@gmail.com>
2026-08-21 17:15:13 -0300

    a

--- Protótipo 03/Backend/jornal/src/data/prisma.js
+++ Protótipo 03/Backend/jornal/src/data/prisma.js
@@ -1,7 +1,8 @@
 const { PrismaClient } = require("@prisma/client");
-const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
+const PrismaPg = require("@prisma/adapter-pg");
+//const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
 
-const adapter = new PrismaMariaDb(process.env.DATABASE_URL);
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
 const prisma = new PrismaClient({ adapter });
 
