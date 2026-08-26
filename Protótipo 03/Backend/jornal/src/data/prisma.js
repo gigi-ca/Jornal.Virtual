1089787deeae83173ac9d73191a945914cbe460a
ClaraAntonacci <andrzejewskyantonacciclara@gmail.com>
2026-08-26 16:53:41 -0300

    atualização banco vercel

--- Protótipo 03/Backend/jornal/src/data/prisma.js
+++ Protótipo 03/Backend/jornal/src/data/prisma.js
@@ -1,9 +1,12 @@
 const { PrismaClient } = require("@prisma/client");
-const PrismaPg = require("@prisma/adapter-pg");
-//const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
+const { PrismaPg } = require("@prisma/adapter-pg");
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL
+});
 
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({
+    adapter
+});
 
 module.exports = prisma;
