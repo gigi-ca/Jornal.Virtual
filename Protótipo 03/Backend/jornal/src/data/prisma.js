705b4014bda1ff851460a3e15c66cdb0775210dd
biaams-sys <biaams16@gmail.com>
2026-09-23 07:57:25 -0300

    back arrumou!

--- Protótipo 03/Backend/jornal/src/data/prisma.js
+++ Protótipo 03/Backend/jornal/src/data/prisma.js
@@ -1,17 +1,15 @@
 const { PrismaClient } = require("@prisma/client");
 const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
 
-<<<<<<< HEAD
-const adapter = new PrismaMariaDb({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
-=======
+// const adapter = new PrismaMariaDb({
+//     host: process.env.DB_HOST,
+//     port: Number(process.env.DB_PORT),
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
+
 const adapter = new PrismaMariaDb(process.env.DATABASE_URL);
->>>>>>> 3aa25da7ffcd96f2726ca0befa938173ba14cab3
 
 const prisma = new PrismaClient({
     adapter
